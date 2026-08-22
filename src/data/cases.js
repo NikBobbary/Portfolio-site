8f5b483f95888fcb0dceb852149f6c95eb95ce2c
NikBobbary <nikithabobbary178@gmail.com>
2026-08-23 01:23:08 +0530

    0123

---
+++ src/data/cases.js
@@ -0,0 +1,23 @@
+export const CASE_STUDIES = [
+  {
+    id: "dyocar",
+    name: "Dyocar",
+    href: "/project/dyocar",
+    src: "/Screens/2400/dyocars-2.jpg",
+    chips: ["Marketplace", "Discovery"],
+  },
+  {
+    id: "pairty",
+    name: "Pairty",
+    href: "/project/pairty",
+    src: "/Screens/2400/Pairty-1.jpg",
+    chips: ["Identity Systems", "Network"],
+  },
+  {
+    id: "lessonpal",
+    name: "Lessonpal",
+    href: "/project/lessonpal",
+    src: "/Screens/2400/lessonpal-1.jpg",
+    chips: ["EdTech", "Marketplace"],
+  },
+];
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
+import Reveal from "./components/Reveal.jsx";
 import ProjectLore from "./pages/ProjectLore.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Reveal />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/project/lore" element={<ProjectLore />} />
